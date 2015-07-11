47c019abe104a763961f32de17dcaaf4d9ada633
shaimuli <shaimuli@gmail.com>
2015-07-12 00:04:52 +0300

    45345
    345345

--- bin/Android/Debug/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Users\shai\Documents\Visual Studio 2015\Projects\NFCExmample\NFCExmample\platforms\android\ant-build\AndroidManifest.xml : \
-C:\Users\shai\Documents\Visual Studio 2015\Projects\NFCExmample\NFCExmample\platforms\android\AndroidManifest.xml \
--- bin/Android/Debug/MainActivity-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-C:\Users\shai\Documents\Visual Studio 2015\Projects\NFCExmample\NFCExmample\platforms\android\ant-build\MainActivity-debug-unaligned.apk : \
-C:\Users\shai\Documents\Visual Studio 2015\Projects\NFCExmample\NFCExmample\platforms\android\ant-build\MainActivity.ap_ \
-C:\Users\shai\Documents\Visual Studio 2015\Projects\NFCExmample\NFCExmample\platforms\android\ant-build\classes.dex \
-C:\Users\shai\Documents\Visual Studio 2015\Projects\NFCExmample\NFCExmample\platforms\android\CordovaLib\ant-build\classes.jar \
